947cbbf1780b156eff22085c3d36e89cd613eb2e
bug_hunter <76158483+zartabkhalil@users.noreply.github.com>
2026-06-26 13:16:51 +0500

    envrionment

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
-import 'dotenv/config'; // MUST be first — loads .env before any other module runs
 import cors from "cors";
+import "dotenv/config"; // MUST be first — loads .env before any other module runs
 import express from "express";
+import mongoSanitize from "express-mongo-sanitize";
+import helmet from "helmet";
 import connectDb from "./src/config/db.js";
-import userRouter from "./src/routes/userroutes.js";
 import errorHandler from "./src/middlewares/error.middleware.js";
 import { generalLimiter } from "./src/middlewares/rateLimit.middleware.js";
+import userRouter from "./src/routes/userroutes.js";
 const app = express();
 connectDb();
 const PORT = process.env.PORT || 8086;
 app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
 app.use(express.json());
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
-
-//rate limiter 
-app.use(generalLimiter)
-
+//rate limiter
+app.use(generalLimiter);
 
 //register routes
 app.use("/api/user/", userRouter);
 
 //middlewar for error handling
-app.use(errorHandler)
-
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on http:localhost:${PORT}`);
 });
-
-
-
